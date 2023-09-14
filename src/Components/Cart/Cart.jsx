a2d3280d2f68edd8c27983cf4be06ec89b4fbc73
Mehedy-Tanvir <mehedytanvir451@gmail.com>
2023-09-14 15:45:56 +0600

    cost and remaining functionality added

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,14 +1,16 @@
 import PropTypes from "prop-types";
-const Cart = ({ hiredArtists }) => {
+const Cart = ({ hiredArtists, price, remaining }) => {
   return (
     <div>
       <p className="text-2xl font-bold">Budget: 30000 $</p>
-      <p className="text-2xl font-bold">Cost: 30000 $</p>
-      <p className="text-2xl font-bold">Remaining: 30000 $</p>
-      <p className="text-2xl font-bold">Hired Artists</p>
-      <ol>
-        {hiredArtists.map((hiredArtist) => (
-          <li key={hiredArtist.id}>{hiredArtist.name}</li>
+      <p className="text-2xl font-bold">Cost: {price} $</p>
+      <p className="text-2xl font-bold">Remaining: {remaining} $</p>
+      <p className="text-2xl font-bold">Hired Artists {hiredArtists.length}</p>
+      <ol className="mt-2 ml-4 text-left">
+        {hiredArtists.map((hiredArtist, idx) => (
+          <li key={idx}>
+            {hiredArtist.name} - {hiredArtist.salary}$
+          </li>
         ))}
       </ol>
     </div>
@@ -16,5 +18,7 @@ const Cart = ({ hiredArtists }) => {
 };
 Cart.propTypes = {
   hiredArtists: PropTypes.array.isRequired,
+  price: PropTypes.number.isRequired,
+  remaining: PropTypes.number.isRequired,
 };
 export default Cart;
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -6,19 +6,42 @@ import Cart from "../Cart/Cart";
 const Home = () => {
   const [artists, setArtists] = useState([]);
   const [hiredArtists, setHiredArtists] = useState([]);
+  const [remaining, setRemaining] = useState(30000);
+  const [price, setPrice] = useState(0);
   useEffect(() => {
     fetch("./data.json")
       .then((res) => res.json())
       .then((data) => setArtists(data));
   }, []);
+
   const handleHire = (artist) => {
-    const newHiredArtists = [...hiredArtists, artist];
-    setHiredArtists(newHiredArtists);
+    {
+      if (hiredArtists.includes(artist)) {
+        return alert("Already booked.");
+      } else {
+        let cost = artist.salary;
+        hiredArtists.forEach((item) => (cost = cost + item.salary));
+        const newRemaining = 30000 - cost;
+        if (cost > 30000) {
+          return alert("Budget exceeded.");
+        } else {
+          setPrice(cost);
+          const newHiredArtists = [...hiredArtists, artist];
+          setHiredArtists(newHiredArtists);
+          setRemaining(newRemaining);
+        }
+      }
+    }
   };
+
   return (
     <div className="flex flex-col gap-4 md:flex-row">
       <Cards artists={artists} handleHire={handleHire}></Cards>
-      <Cart hiredArtists={hiredArtists}></Cart>
+      <Cart
+        remaining={remaining}
+        price={price}
+        hiredArtists={hiredArtists}
+      ></Cart>
     </div>
   );
 };
